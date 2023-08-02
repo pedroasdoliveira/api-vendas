55014baac1efa765ea71a77001c08f223f967184
Pedro Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2023-08-02 02:03:03 -0300

    feat: add new Users route

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 
+import usersRouter from "@modules/users/routes/users.routes";
 import productsRouter from "@modules/products/routes/products.routes";
 
 const routes = Router();
+
+routes.use('/users', usersRouter)
 
 routes.use('/products', productsRouter);
 

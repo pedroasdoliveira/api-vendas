59ca1ec633cd1464518d516f828f0ff0c3f5d6c9
Pedro Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2023-08-01 23:55:35 -0300

    feat: implement erros validations with celebrate

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate'; // Lib para validação de dados de Requisição
+
 import ProductsController from '../controller/ProductsController';
 
 const productsRouter = Router();
@@ -7,12 +9,43 @@ const productsController = new ProductsController();
 
 productsRouter.get('/', productsController.index);
 
-productsRouter.post('/', productsController.create);
+productsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productsController.create,
+);
 
-productsRouter.get('/:id', productsController.show);
+productsRouter.get(
+  '/:id',
+  celebrate({ [Segments.PARAMS]: { id: Joi.string().uuid().required } }),
+  productsController.show,
+);
 
-productsRouter.put('/:id', productsController.update);
+productsRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required,
+    },
+  }),
+  productsController.update,
+);
 
-productsRouter.delete('/:id', productsController.delete);
+productsRouter.delete(
+  '/:id',
+  celebrate({ [Segments.PARAMS]: { id: Joi.string().uuid().required } }),
+  productsController.delete,
+);
 
 export default productsRouter;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,9 @@
 import 'reflect-metadata';
 
+import "express-async-errors";
 import cors from 'cors';
 import express from 'express';
-import "express-async-errors";
+import { errors } from 'celebrate';
 
 import Errors from '@shared/middlewares/Errors';
 import routes from './routes';
@@ -19,6 +20,9 @@ app.use(
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors())
+
 app.use(Errors);
 
 const PORT = process.env.PORT || 3333;
